d90e98e0115374d095da4ef11e78ffcb0612206d
anju1511 <anjumathew1511@gmail.com>
2021-11-04 16:45:30 +0530

    third commit

--- shoppingcart/shopeapp/urls.py
+++ shoppingcart/shopeapp/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('product_reg',views.ProductRegister,name='product_reg')
+    path('product_reg',views.ProductRegister,name='product_reg'),
+    path('product_reg/<int:pid>',views.ProductRegister,name='product_reg'),
+    path('product_list/',views.ProductList,name='product_list'),
+    path('product_delete/<int:pid>',views.ProductDelete,name='product_delete'),
+    path('product_edit/<int:pid>',views.ProductEdit,name='product_edit'),
     
 ]
--- shoppingcart/shopeapp/views.py
+++ shoppingcart/shopeapp/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Product
 
 # Create your views here.
-def ProductRegister(request):
+def ProductRegister(request,pid=None):
+    if pid:
+        product=Product.objects.get(id=pid)
+        return render(request,"product_reg.html",{'product':product})
     if request.method == 'POST':
         product_name=request.POST.get('pname')
         product_price=request.POST.get('pprice')
@@ -19,7 +22,38 @@ def ProductRegister(request):
                         product_image=product_image,
                         product_status= p_status)
         product.save()
+        
+        return redirect(ProductList)
 
     return render(request,"product_reg.html")
 def ProductList(request):
-    return render(request,"product_list.html")
+    product=Product.objects.all()
+    context={
+        "product":product
+    }
+    return render(request,"product_list.html",context)
+def ProductDelete(request,pid):
+    print(pid)
+    product=Product.objects.filter(id=pid)
+    product.delete()
+    return redirect(ProductList)
+def ProductEdit(request,pid):
+    if request.method =="POST":
+        product_name=request.POST.get('pname')
+        product_price=request.POST.get('pprice')
+        product_quantity=request.POST.get('pquantity')
+        try:
+            product_image=request.FILES['pimage']
+        except Exception:
+            product_image=Product.objects.get(id=pid).product_image
+        product_status=request.POST.get('pstatus')
+        if product_status == "0":
+            p_status="In-stock"
+        else:
+            p_status="out of stock"
+        product=Product.objects.filter(id=pid).update(product_name=product_name,
+                        product_quantity=product_quantity,
+                        product_price=product_price,
+                        product_image=product_image,
+                        product_status= p_status)
+        return redirect(ProductList)
