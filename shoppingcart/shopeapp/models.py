fe20d6a14731760415c2be1b5b10078cfa040b88
anju1511 <anjumathew1511@gmail.com>
2021-11-04 15:54:30 +0530

    second commit

--- shoppingcart/shopeapp/models.py
+++ shoppingcart/shopeapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    product_name=models.CharField(max_length=50)
+    product_quantity=models.IntegerField()
+    product_price=models.CharField(max_length=50)
+    product_image=models.ImageField(upload_to='images/')
+    product_status=models.CharField(max_length=50)
--- shoppingcart/shopeapp/views.py
+++ shoppingcart/shopeapp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def ProductRegister(request):
+    if request.method == 'POST':
+        product_name=request.POST.get('pname')
+        product_price=request.POST.get('pprice')
+        product_quantity=request.POST.get('pquantity')
+        product_image=request.FILES['pimage']
+        product_status=request.POST.get('pstatus')
+        if product_status == "0":
+            p_status="In-stock"
+        else:
+            p_status="out of stock"
+        product=Product(product_name=product_name,
+                        product_quantity=product_quantity,
+                        product_price=product_price,
+                        product_image=product_image,
+                        product_status= p_status)
+        product.save()
+
+    return render(request,"product_reg.html")
+def ProductList(request):
+    return render(request,"product_list.html")
